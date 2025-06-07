feae4b4cc19611d5627dcac37f1be87550548de2
nemo <ymillet.ping@gmail.com>
2025-06-07 02:14:02 +0200

    Quick menu
    ajout de placeholders dans le frontend pour les futurs onglets
    les fichiers n'existent pas encore

--- frontend/app.py
+++ frontend/app.py
@@ -4,9 +4,26 @@ import requests
 from training_tab import training_tab
 from user_tab import user_tab
 
-tab = st.sidebar.selectbox("Choose Tab", ["User", "Training"])
+st.sidebar.title("OpenAMS")
+tab = st.sidebar.radio(
+    "Menu", 
+    ("Home", "Athlete", "Training", "Health", "Settings")
+)
 
-if tab == "User":
+
+if tab == "Home":
+    st.subheader("")
     user_tab()
+
+elif tab == "Athlete":
+    st.subheader("Athlete")
+
 elif tab == "Training":
+    st.subheader("Training Logs")
     training_tab()
+
+elif tab == "Health":
+    st.subheader("Health")
+    
+elif tab == "Settings":
+    st.subheader("Settings")
